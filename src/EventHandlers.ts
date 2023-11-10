0e2d8e979a8e44bd6c45c6f7c5e68b2ba99d2c92
denhampreen <denhampreen@gmail.com>
2023-11-10 17:26:35 +0200

    feat: poolCreated event & Factory entity poolCreated event

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -8,6 +8,8 @@
 import {
   UniV3FactoryContract_OwnerChanged_loader,
   UniV3FactoryContract_OwnerChanged_handler,
+  UniV3FactoryContract_PoolCreated_loader,
+  UniV3FactoryContract_PoolCreated_handler,
 } from "../generated/src/Handlers.gen";
 
 import { FactoryEntity } from "../generated/src/Types.gen";
@@ -26,7 +28,32 @@ UniV3FactoryContract_OwnerChanged_handler(({ event, context }) => {
   let factoryObject: FactoryEntity = {
     id: factoryAddress,
     owner: newFactoryOwner,
+    poolCount: 0n,
   };
 
   context.Factory.set(factoryObject);
+});
+
+// event event PoolCreated(address indexed pool, address indexed token0, address indexed token1, uint24 fee, int24 tickSpacing)
+UniV3FactoryContract_PoolCreated_loader(({ event, context }) => {
+  let factoryAddress = event.srcAddress;
+  context.Factory.load(factoryAddress);
+});
+
+UniV3FactoryContract_PoolCreated_handler(({ event, context }) => {
+  let factoryAddress = event.srcAddress;
+  let factory = context.Factory.get(factoryAddress);
+
+  if (factory == null) {
+    context.log.error(
+      "Since the ownerChanged event is emitted in the constructor, it should be impossible for this to happen"
+    );
+  } else {
+    let factoryObject: FactoryEntity = {
+      ...factory,
+      poolCount: factory.poolCount + 1n,
+    };
+
+    context.Factory.set(factoryObject);
+  }
 });
