84446c20de61511765494b6bae96560c11108de3
normdoow <noahbragg@cedarville.edu>
2023-11-10 14:57:05 -0800

    updated the factory name

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -6,10 +6,10 @@
   */
 
 import {
-  UniV3FactoryContract_OwnerChanged_loader,
-  UniV3FactoryContract_OwnerChanged_handler,
-  UniV3FactoryContract_PoolCreated_loader,
-  UniV3FactoryContract_PoolCreated_handler,
+  FactoryContract_OwnerChanged_loader,
+  FactoryContract_OwnerChanged_handler,
+  FactoryContract_PoolCreated_loader,
+  FactoryContract_PoolCreated_handler,
   PoolContract_Initialize_loader,
   PoolContract_Initialize_handler,
 } from "../generated/src/Handlers.gen";
@@ -18,12 +18,12 @@ import { FactoryEntity, PoolEntity } from "../generated/src/Types.gen";
 import { addressToDex } from "./utils/addressToDex";
 
 // event OwnerChanged(address indexed oldOwner, address indexed newOwner)
-UniV3FactoryContract_OwnerChanged_loader(({ event, context }) => {
+FactoryContract_OwnerChanged_loader(({ event, context }) => {
   let factoryAddress = event.srcAddress;
   context.Factory.load(factoryAddress);
 });
 
-UniV3FactoryContract_OwnerChanged_handler(({ event, context }) => {
+FactoryContract_OwnerChanged_handler(({ event, context }) => {
   let factoryAddress = event.srcAddress;
   let newFactoryOwner = event.params.newOwner;
   let factory = context.Factory.get(factoryAddress);
@@ -39,14 +39,14 @@ UniV3FactoryContract_OwnerChanged_handler(({ event, context }) => {
 });
 
 // event event PoolCreated(address indexed pool, address indexed token0, address indexed token1, uint24 fee, int24 tickSpacing)
-UniV3FactoryContract_PoolCreated_loader(({ event, context }) => {
+FactoryContract_PoolCreated_loader(({ event, context }) => {
   let factoryAddress = event.srcAddress;
   context.Factory.load(factoryAddress);
   // used to register dynamic contracts ie. contracts that are registered at runtime
   context.contractRegistration.addPool(event.params.pool);
 });
 
-UniV3FactoryContract_PoolCreated_handler(({ event, context }) => {
+FactoryContract_PoolCreated_handler(({ event, context }) => {
   let factoryAddress = event.srcAddress;
   let factory = context.Factory.get(factoryAddress);
 
