e79c2682b155ca3917d0cd6c94ef88f673948fca
normdoow <noahbragg@cedarville.edu>
2023-11-14 16:54:55 -0800

    added more pools and setting the dex for a pool

--- scripts/poolsToJSON.js
+++ scripts/poolsToJSON.js
@@ -1,14 +1,42 @@
 const { querySubgraph } = require("../src/utils/querySubgraph");
-const fs =  require("fs");
+const fs = require("fs");
+
+const dexKeys = {
+  uniswap: "uniswap",
+  pancakeswap: "pancakeswap",
+};
+
+const subgraphs = [
+  {
+    endpoint: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
+    key: dexKeys.uniswap,
+  },
+  {
+    endpoint:
+      "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-bsc",
+    key: dexKeys.uniswap,
+  },
+  {
+    endpoint:
+      "https://api.thegraph.com/subgraphs/name/pancakeswap/exchange-v3-eth",
+    key: dexKeys.pancakeswap,
+  },
+  {
+    endpoint:
+      "https://api.thegraph.com/subgraphs/name/pancakeswap/exchange-v3-bsc",
+    key: dexKeys.pancakeswap,
+  },
+];
 
 async function poolsToJSON() {
   const processedData = {};
 
-  let page = 0;
-  let uniswapData = {};
-  do {
-    uniswapData = await querySubgraph(
-      `{
+  for (let subgraph of subgraphs) {
+    let page = 0;
+    let response = {};
+    do {
+      response = await querySubgraph(
+        `{
         pools (first: 300, skip: ${
           page * 300
         }, orderBy: totalValueLockedUSD, orderDirection: desc, where: {liquidity_gt: 0, totalValueLockedUSD_gte: 1000, volumeUSD_gte: 10000}) {
@@ -16,21 +44,21 @@ async function poolsToJSON() {
         }
       }
   `,
-      "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
-    );
-    console.log("uniswapData", uniswapData)
-    if(uniswapData.data.pools) {
-      for (let pool of uniswapData.data.pools) {
-        processedData[pool.id] = "uniswap";
+        subgraph.endpoint
+      );
+      console.log("uniswapData", response);
+      if (response.data.pools) {
+        for (let pool of response.data.pools) {
+          processedData[pool.id] = subgraph.key;
+        }
       }
-    }
-    page++;
-  } while (uniswapData.data.pools && uniswapData.data.pools.length > 0);
+      page++;
+    } while (response.data.pools && response.data.pools.length > 0);
+  }
 
   const filepath = "./src/poolAddressToDex.json";
-  console.log('data', processedData)
   fs.writeFileSync(filepath, JSON.stringify(processedData, 2, 2));
-  console.log('Wrote pools out to ./src/poolAddressToDex.json')
+  console.log("Wrote pools out to ./src/poolAddressToDex.json");
 }
 
 poolsToJSON();
--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -16,6 +16,9 @@ import {
 
 import { FactoryEntity, PoolEntity } from "../generated/src/Types.gen";
 import { addressToDex } from "./utils/addressToDex";
+import poolAddressToDex from "./poolAddressToDex.json";
+
+type JSON = { [key: string]: string };
 
 // event OwnerChanged(address indexed oldOwner, address indexed newOwner)
 FactoryContract_OwnerChanged_loader(({ event, context }) => {
@@ -27,6 +30,8 @@ FactoryContract_OwnerChanged_handler(({ event, context }) => {
   let factoryAddress = event.srcAddress;
   let newFactoryOwner = event.params.newOwner;
   let factory = context.Factory.get(factoryAddress);
+
+  context.log.info("did get factory address " + addressToDex(event.srcAddress));
 
   let factoryObject: FactoryEntity = {
     id: factoryAddress,
@@ -70,13 +75,18 @@ PoolContract_Initialize_loader(({ event, context }) => {}); // don't need a load
 PoolContract_Initialize_handler(({ event, context }) => {
   let tick = event.params.tick;
   let sqrtPrice = event.params.sqrtPriceX96;
+  context.log.info("poolAddressToDex: " + event.srcAddress);
+
+  if (!(poolAddressToDex as JSON)[event.srcAddress]) {
+    return null;
+  }
 
   let poolObject: PoolEntity = {
     id: event.srcAddress,
     createdAtTimestamp: event.blockTimestamp, // can see this list of available properties here https://docs.envio.dev/docs/event-handlers
     sqrtPrice: sqrtPrice,
     tick: tick,
-    // dexKey: addressToDex(event.srcAddress),
+    dexKey: (poolAddressToDex as JSON)[event.srcAddress],
   };
 
   context.Pool.set(poolObject);
