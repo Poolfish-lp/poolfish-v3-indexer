d12f60efcaadcc293da9012930760b9ea82f9a96
denhampreen <denhampreen@gmail.com>
2024-02-12 08:02:27 +0200

    feat: contract calls for token data with caching

--- src/FactoryEventHandlers.ts
+++ src/FactoryEventHandlers.ts
@@ -1,6 +1,6 @@
 import {
     FactoryContract_PoolCreated_loader,
-    FactoryContract_PoolCreated_handler,
+    FactoryContract_PoolCreated_handlerAsync,
 } from '../generated/src/Handlers.gen'
 
 import {
@@ -10,96 +10,68 @@ import {
 } from '../generated/src/Types.gen'
 import { ZERO_BD, ZERO_BI } from './constants'
 import { addressToDex } from './utils/addressToDex'
-import { getPoolAddressToInfo } from './utils/getPoolAddressToInfo'
+
+import { getOrCreateToken_async } from './async/token'
 
 // event event PoolCreated(address indexed pool, address indexed token0, address indexed token1, uint24 fee, int24 tickSpacing)
-FactoryContract_PoolCreated_loader(({ event, context }) => {
+FactoryContract_PoolCreated_loader(async ({ event, context }) => {
     let factoryAddress = event.srcAddress
-    context.Factory.load(factoryAddress)
+    await context.Factory.load(factoryAddress)
     // used to register dynamic contracts ie. contracts that are registered at runtime
-    context.contractRegistration.addPool(event.params.pool)
+    await context.contractRegistration.addPool(event.params.pool)
 })
 
-FactoryContract_PoolCreated_handler(({ event, context }) => {
+FactoryContract_PoolCreated_handlerAsync(async ({ event, context }) => {
     let factoryAddress = event.srcAddress
-    let factory = context.Factory.get(factoryAddress)
+    let factory = await context.Factory.get(factoryAddress)
 
-    const poolInfo = getPoolAddressToInfo(event.params.pool)
-
-    if (!poolInfo) {
-        context.log.info('no pool info for ' + event.params.pool)
-        return
-    }
-
-    const dexKey = poolInfo.dexKey
+    const dexKey = addressToDex(event.srcAddress)
 
     if (factory == null) {
         let factoryObject: FactoryEntity = {
             id: factoryAddress,
             poolCount: 0n,
-            dexKey: addressToDex(event.srcAddress),
+            dexKey: dexKey,
         }
-        context.Factory.set(factoryObject)
+        await context.Factory.set(factoryObject)
 
         let bundleObject = {
             id: '1',
             ethPriceUSD: ZERO_BD,
         }
-        context.Bundle.set(bundleObject)
+        await context.Bundle.set(bundleObject)
     } else {
         let factoryObject: FactoryEntity = {
             ...factory,
             poolCount: factory.poolCount + 1n,
         }
 
-        context.Factory.set(factoryObject)
+        await context.Factory.set(factoryObject)
     }
 
     // let decimals = 6n; //fetchTokenDecimals(event.params.token0);
     // bail if we couldn't figure out the decimals
     // if (decimals === null) {
-    //   context.log.info("mybug the decimal on token 0 was null");
+    //   await context.log.info("mybug the decimal on token 0 was null");
     //   return;
     // }
 
     // create tokens
-    let token0Object: TokenEntity = {
-        id: event.params.token0.toLowerCase(),
-        symbol: poolInfo.token0.symbol, //fetchTokenSymbol(event.params.token0),
-        name: poolInfo.token0.name, //fetchTokenName(event.params.token0),
-        totalSupply: BigInt(poolInfo.token0.totalSupply), //fetchTokenTotalSupply(event.params.token0),
-        decimals: BigInt(poolInfo.token0.decimals),
-        derivedETH: ZERO_BD,
-        volume: ZERO_BD,
-        volumeUSD: ZERO_BD,
-        feesUSD: ZERO_BD,
-        untrackedVolumeUSD: ZERO_BD,
-        totalValueLocked: ZERO_BD,
-        totalValueLockedUSD: ZERO_BD,
-        totalValueLockedUSDUntracked: ZERO_BD,
-        txCount: ZERO_BI,
-        poolCount: ZERO_BI,
-    }
-    context.Token.set(token0Object)
+    let token0Object: TokenEntity = await getOrCreateToken_async(
+        event,
+        event.params.token0,
+        context.Token.get,
+    )
 
-    let token1Object: TokenEntity = {
-        id: event.params.token1.toLowerCase(),
-        symbol: poolInfo.token1.symbol, //fetchTokenSymbol(event.params.token0),
-        name: poolInfo.token1.name, //fetchTokenName(event.params.token0),
-        totalSupply: BigInt(poolInfo.token1.totalSupply), //fetchTokenTotalSupply(event.params.token0),
-        decimals: BigInt(poolInfo.token1.decimals),
-        derivedETH: ZERO_BD,
-        volume: ZERO_BD,
-        volumeUSD: ZERO_BD,
-        feesUSD: ZERO_BD,
-        untrackedVolumeUSD: ZERO_BD,
-        totalValueLocked: ZERO_BD,
-        totalValueLockedUSD: ZERO_BD,
-        totalValueLockedUSDUntracked: ZERO_BD,
-        txCount: ZERO_BI,
-        poolCount: ZERO_BI,
-    }
-    context.Token.set(token1Object)
+    await context.Token.set(token0Object)
+
+    let token1Object: TokenEntity = await getOrCreateToken_async(
+        event,
+        event.params.token1,
+        context.Token.get,
+    )
+
+    await context.Token.set(token1Object)
 
     let poolObject: PoolEntity = {
         id: event.params.pool,
@@ -140,5 +112,5 @@ FactoryContract_PoolCreated_handler(({ event, context }) => {
     //   token1: token1Object.id,
     // };
 
-    context.Pool.set(poolObject)
+    await context.Pool.set(poolObject)
 })
---
+++ src/async/framework.ts
@@ -0,0 +1,183 @@
+import { createPublicClient, getContract, fallback, http } from 'viem'
+import {
+    arbitrum,
+    avalanche,
+    base,
+    bsc,
+    gnosis,
+    mainnet,
+    optimism,
+    polygon,
+    scroll,
+    sepolia,
+    Chain,
+} from 'viem/chains'
+import { Address } from '../types'
+import ERC20 from '../../abis/ERC20.json'
+import ERC20Bytes from '../../abis/ERC20Bytes.json'
+
+type networks = { chain: Chain; rpcEndpoints: Array<string> }
+
+/** all rpc endpoints are public */
+const supported: Array<networks> = [
+    {
+        chain: mainnet,
+        rpcEndpoints: [
+            'https://rpc.ankr.com/eth',
+            'https://eth.llamarpc.com',
+            'https://ethereum.publicnode.com',
+            'https://eth-mainnet.public.blastapi.io',
+            'https://rpc.tornadoeth.cash/eth',
+        ],
+    },
+    {
+        chain: arbitrum,
+        rpcEndpoints: [
+            'https://arbitrum.llamarpc.com',
+            'https://arb-pokt.nodies.app',
+            'https://endpoints.omniatech.io/v1/arbitrum/one/public',
+            'https://rpc.tornadoeth.cash/arbitrum',
+            'https://rpc.ankr.com/arbitrum',
+        ],
+    },
+    {
+        chain: avalanche,
+        rpcEndpoints: [
+            'https://avalanche.public-rpc.com',
+            'https://avalanche.drpc.org',
+            'https://api.avax.network/ext/bc/C/rpc',
+            'https://rpc.ankr.com/avalanche',
+            'https://avax.meowrpc.com',
+        ],
+    },
+    {
+        chain: base,
+        rpcEndpoints: [
+            'https://base.llamarpc.com',
+            'https://base-pokt.nodies.app',
+            'https://base-mainnet.public.blastapi.io',
+            'https://base.drpc.org',
+            'https://base.meowrpc.com',
+        ],
+    },
+    {
+        chain: bsc,
+        rpcEndpoints: [
+            'https://binance.llamarpc.com',
+            'https://binance.nodereal.io',
+            'https://rpc.ankr.com/bsc',
+            'https://rpc.tornadoeth.cash/bsc',
+            'https://bsc-mainnet.public.blastapi.io',
+        ],
+    },
+    {
+        chain: gnosis,
+        rpcEndpoints: [
+            'https://rpc.ankr.com/gnosis',
+            'https://gnosis-mainnet.public.blastapi.io',
+            'https://rpc.gnosischain.com',
+            'https://gnosis.drpc.org',
+            'https://rpc.tornadoeth.cash/gnosis',
+        ],
+    },
+
+    {
+        chain: optimism,
+        rpcEndpoints: [
+            'https://optimism.llamarpc.com',
+            'https://rpc.ankr.com/optimism',
+            'https://optimism.publicnode.com',
+            'https://optimism.meowrpc.com',
+            'https://rpc.tornadoeth.cash/optimism',
+        ],
+    },
+    {
+        chain: polygon,
+        rpcEndpoints: [
+            'https://polygon.llamarpc.com',
+            'https://polygon-mainnet.public.blastapi.io',
+            'https://polygon.drpc.org',
+            'https://polygon-rpc.com',
+            'https://rpc.ankr.com/polygon',
+        ],
+    },
+    {
+        chain: scroll,
+        rpcEndpoints: [
+            'https://rpc.ankr.com/scroll',
+            'https://rpc-scroll.icecreamswap.com',
+            'https://scroll-mainnet.public.blastapi.io',
+            'https://rpc.scroll.io',
+            'https://scroll-mainnet.chainstacklabs.com',
+        ],
+    },
+    {
+        chain: sepolia,
+        rpcEndpoints: [
+            'https://gateway.tenderly.co/public/sepolia',
+            'https://ethereum-sepolia.blockpi.network/v1/rpc/public',
+            'https://1rpc.io/sepolia',
+            'https://gateway.tenderly.co/public/sepolia',
+            'https://rpc2.sepolia.org',
+        ],
+    },
+]
+
+const clients = supported.map((network) =>
+    createPublicClient({
+        batch: {
+            multicall: true,
+        },
+        chain: network.chain,
+        transport: fallback(
+            // todo: bind to Infura/Alchemy for production
+            network.rpcEndpoints.map((endpoint) => http(endpoint)),
+            {
+                rank: true, // https://viem.sh/docs/clients/transports/fallback#rank-optional
+                retryCount: 5,
+            },
+        ),
+    }),
+)
+
+function getClient(chainId: number | string | bigint) {
+    const client = clients.find(
+        (c) => c.chain.id.toString() === chainId.toString(),
+    )
+
+    if (!client) {
+        throw new Error(
+            `Client missing from configuration for chain: ${chainId}`,
+        )
+    }
+
+    return client
+}
+
+function getERC20Contract(
+    address: Address,
+    client: ReturnType<typeof getClient>,
+) {
+    return getContract({
+        address: address as `0x${string}`,
+        abi: ERC20,
+        publicClient: client,
+    })
+}
+
+function getERC20BytesContract(
+    address: Address,
+    client: ReturnType<typeof getClient>,
+) {
+    return getContract({
+        address: address as `0x${string}`,
+        abi: ERC20Bytes,
+        publicClient: client,
+    })
+}
+
+export const framework = {
+    getClient,
+    getERC20Contract,
+    getERC20BytesContract,
+}
