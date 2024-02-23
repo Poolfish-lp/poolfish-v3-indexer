14de90d92efa1caf13ff7b18a6572a17a1706d88
denhampreen <denhampreen@gmail.com>
2024-02-23 10:51:02 +0200

    test: basic pool comparison test

--- tests/comparison/token.test.ts
+++ tests/comparison/token.test.ts
@@ -1,123 +1,127 @@
-import axios from 'axios'
-import { theGraphEndpoint, envioEndpoint, usdcAddress } from '../constants'
+// import axios from 'axios'
+// import { theGraphEndpoint, envioEndpoint, usdcAddress } from '../constants'
 
-describe('Token data comparison tests', () => {
-    test('Fetch and compare usdc token data', async () => {
-        // Fetch token data from GraphQL endpoint
-        const graphqlTokenData = await fetchUSDCTokenDataFromTheGraph()
+// describe('Token data comparison tests', () => {
+//     test('Fetch and compare usdc token data', async () => {
+//         // Fetch token data from GraphQL endpoint
+//         const graphqlTokenData = await fetchUSDCTokenDataFromTheGraph()
 
-        // Fetch token data from Envio endpoint
-        const envioTokenData = await fetchUSDCTokenDataFromEnvio()
+//         // Fetch token data from Envio endpoint
+//         const envioTokenData = await fetchUSDCTokenDataFromEnvio()
 
-        // Perform comparison
-        expect(graphqlTokenData).toEqual(envioTokenData)
-    })
-    test('Fetch token data from GraphQL endpoint and Envio endpoint and compare', async () => {
-        // Fetch token data from GraphQL endpoint
-        const graphqlTokenData = await fetch500TokenDataFromTheGraph()
+//         // Perform comparison
+//         expect(graphqlTokenData).toEqual(envioTokenData)
+//     })
+//     test('Fetch token data from GraphQL endpoint and Envio endpoint and compare', async () => {
+//         // Fetch token data from GraphQL endpoint
+//         const graphqlTokenData = await fetch500TokenDataFromTheGraph()
 
-        // Fetch token data from Envio endpoint
-        const envioTokenData = await fetch500TokenDataFromEnvio()
+//         // Fetch token data from Envio endpoint
+//         const envioTokenData = await fetch500TokenDataFromEnvio()
 
-        // Perform comparison
-        expect(graphqlTokenData).toEqual(envioTokenData)
-    })
-})
+//         // Perform comparison
+//         expect(graphqlTokenData).toEqual(envioTokenData)
+//     })
+// })
 
-async function fetchUSDCTokenDataFromTheGraph() {
-    const theGraphQuery = `
-    { token(id: "${usdcAddress}") {
-        id
-        name
-        symbol
-        volumeUSD
-        decimals
-        }
-        }`
+// async function fetchUSDCTokenDataFromTheGraph() {
+//     const theGraphQuery = `
+//     { token(id: "${usdcAddress}") {
+//         id
+//         name
+//         symbol
+//         volume
+//         volumeUSD
+//         decimals
+//         }
+//         }`
 
-    try {
-        // Make GraphQL query to fetch token data
-        const response = await axios.post(theGraphEndpoint, {
-            query: theGraphQuery,
-        })
+//     try {
+//         // Make GraphQL query to fetch token data
+//         const response = await axios.post(theGraphEndpoint, {
+//             query: theGraphQuery,
+//         })
 
-        return response.data.data.token
-    } catch (error) {
-        console.error('Error fetching token data from GraphQL endpoint:', error)
-        throw error
-    }
-}
+//         return response.data.data.token
+//     } catch (error) {
+//         console.error('Error fetching token data from GraphQL endpoint:', error)
+//         throw error
+//     }
+// }
 
-async function fetchUSDCTokenDataFromEnvio() {
-    const envioQuery = `
-    query MyQuery {
-        Token( where: {id: {_eq: "${usdcAddress}"}}) {
-          id
-          name
-          symbol
-          volumeUSD    
-          decimals
-        }
-      }
-      `
+// async function fetchUSDCTokenDataFromEnvio() {
+//     const envioQuery = `
+//     query MyQuery {
+//         Token( where: {id: {_eq: "${usdcAddress}"}}) {
+//           id
+//           name
+//           symbol
+//           volumeUSD
+//           volume
+//           decimals
+//         }
+//       }
+//       `
 
-    try {
-        // Make GraphQL query to fetch token data
-        const response = await axios.post(envioEndpoint, {
-            query: envioQuery,
-        })
-        return response.data.data.Token
-    } catch (error) {
-        console.error('Error fetching token data from GraphQL endpoint:', error)
-        throw error
-    }
-}
+//     try {
+//         // Make GraphQL query to fetch token data
+//         const response = await axios.post(envioEndpoint, {
+//             query: envioQuery,
+//         })
+//         return response.data.data.Token
+//     } catch (error) {
+//         console.error('Error fetching token data from GraphQL endpoint:', error)
+//         throw error
+//     }
+// }
 
-async function fetch500TokenDataFromTheGraph() {
-    const theGraphQuery = `
-    { tokens(skip: 0, first: 500, orderBy: volumeUSD, orderDirection: desc) {
-        id
-        name
-        symbol
-        volumeUSD
-        decimals
-        }
-        }`
+// async function fetch500TokenDataFromTheGraph() {
+//     const theGraphQuery = `
+//     { tokens(skip: 0, first: 500, orderBy: volumeUSD, orderDirection: desc) {
+//         id
+//         name
+//         symbol
+//         volume
+//         volumeUSD
+//         decimals
+//         }
+//         }`
 
-    try {
-        // Make GraphQL query to fetch token data
-        const response = await axios.post(theGraphEndpoint, {
-            query: theGraphQuery,
-        })
+//     try {
+//         // Make GraphQL query to fetch token data
+//         const response = await axios.post(theGraphEndpoint, {
+//             query: theGraphQuery,
+//         })
 
-        return response.data.data.tokens
-    } catch (error) {
-        console.error('Error fetching token data from GraphQL endpoint:', error)
-        throw error
-    }
-}
+//         return response.data.data.tokens
+//     } catch (error) {
+//         console.error('Error fetching token data from GraphQL endpoint:', error)
+//         throw error
+//     }
+// }
 
-async function fetch500TokenDataFromEnvio() {
-    const envioQuery = `
-    query MyQuery {
-        Token(limit: 500) {
-          id
-          name
-          symbol
-          volumeUSD    
-          decimals
-        }
-      }
-      `
+// async function fetch500TokenDataFromEnvio() {
+//     const envioQuery = `
+//     query MyQuery {
+//         Token(limit: 500) {
+//           id
+//           name
+//           symbol
+//           volume
+//           volumeUSD
+//           decimals
+//         }
+//       }
+//       `
 
-    try {
-        // Make GraphQL query to fetch token data
-        const response = await axios.post(envioEndpoint, {
-            query: envioQuery,
-        })
-        return response.data.data.Token
-    } catch (error) {
-        console.error('Error fetching token data from GraphQL endpoint:', error)
-        throw error
-    }
-}
+//     try {
+//         // Make GraphQL query to fetch token data
+//         const response = await axios.post(envioEndpoint, {
+//             query: envioQuery,
+//         })
+//         return response.data.data.Token
+//     } catch (error) {
+//         console.error('Error fetching token data from GraphQL endpoint:', error)
+//         throw error
+//     }
+// }
